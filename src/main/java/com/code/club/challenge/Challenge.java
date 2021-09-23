df735eaedc6e6a022d6da7a42a22dee486e2943f
Mindaugas Jaras (vyv4873) <mindaugas.jaras@teliacompany.com>
2021-09-23 19:24:30 +0300

    IGLOO-0000: Fixed typo in message

--- src/main/java/com/code/club/challenge/Challenge.java
+++ src/main/java/com/code/club/challenge/Challenge.java
@@ -9,12 +9,14 @@ import java.util.Optional;
 public class Challenge {
 
     private static final boolean ACCEPTED = false;
+    private static final String BANNER_VIEW = FileUtil.getFileContent("banner.txt");
+    private static final String ACCEPTED_VIEW = FileUtil.getFileContent("accepted.txt");
+    private static final String REJECTED_VIEW = FileUtil.getFileContent("rejected.txt");
+    private static final String FAILURE_VIEW = FileUtil.getFileContent("failure.txt");
 
     public Challenge() {
-        display(FileUtil.getFileContent("banner.txt"));
-        display(ACCEPTED ?
-                FileUtil.getFileContent("accepted.txt") :
-                FileUtil.getFileContent("rejected.txt"));
+        display(BANNER_VIEW);
+        display(ACCEPTED ? ACCEPTED_VIEW : REJECTED_VIEW);
     }
 
     public Optional<Puzzle> getPuzzle() {
@@ -28,12 +30,15 @@ public class Challenge {
     record Puzzle() {
 
         public void solve(String fibonacci100thNumber) {
+            if (!ACCEPTED) {
+                display(REJECTED_VIEW);
+            }
             var encryptedMessage = FileUtil.getFileContent("encrypted.txt");
             try {
                 var decryptedMessage = EncryptorAesGcmPassword.decrypt(encryptedMessage, fibonacci100thNumber);
                 display(JsonUtil.toJson(decryptedMessage));
             } catch (Exception e) {
-                display(FileUtil.getFileContent("failure.txt"));
+                display(FAILURE_VIEW);
             }
         }
     }
